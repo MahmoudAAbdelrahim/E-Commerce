969bb26d9e12f94048f6d9906e7793639536f5e5
MahmoudAAbdelrahim <mahmoudalimahmoud536@gmail.com>
2025-05-05 20:57:41 +0300

    App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@
 import React, {useState, useEffect} from 'react';
 import { BrowserRouter, Routes, Route, Link,  NavLink } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 import './App.css';
 import Home from './Home'
 import Navebar from './Navebar';
@@ -11,6 +10,7 @@ import RagistrPadg from './RagistrPadg';
 import LoginPadg from './LoginPadg';
 import ChackOut from './ChackOut';
 import Footer from './Footer';
+import ProuductDetils from './ProuductDetils'
 import { ProductProvider } from './ProductContext'; // لو في مجلد ثاني عدل المسار
 
 
@@ -31,7 +31,9 @@ function App() {
       <Route path="/RagistrPadg" element={<RagistrPadg />} />
       <Route path="/LoginPadg" element={<LoginPadg />} />
       <Route path="/ChackOut" element={<ChackOut />} />
+      <Route path="/products/:id" element={<ProuductDetils />} />
       <Route path="/Footer" element={<Footer />} />
+
       </Routes>
       <Footer/>
       </ProductProvider>
