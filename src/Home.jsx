9ea56680a912acbf59d1618f59c74228d6982d44
MahmoudAAbdelrahim <mahmoudalimahmoud536@gmail.com>
2025-05-06 14:12:45 +0300

    Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,7 +4,10 @@ import { ProductContext } from './ProductContext';
 import { Link } from 'react-router-dom';
 import { Button } from 'react-bootstrap';
 import { FaCartPlus } from 'react-icons/fa';
-import './App.css';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import { Navigation, Autoplay } from 'swiper/modules';import './App.css';
 
 const Home = () => {
     const { products, setSelectProduct, addToCart } = useContext(ProductContext);
@@ -14,9 +17,85 @@ const Home = () => {
       const handleaddtocart =(products)=>{
         addToCart(products);
       }
+      const selectedProducts = products && products.length > 0
+      ? products.slice(5, 8)
+      : [{ thumbnail: 'https://via.placeholder.com/600x300?text=No+Image' }];
+
+// products اللي جاي من أول API
+const firstProducts = products.slice(5, 10);  // مثلاً عروض
+// products اللي جاي من تاني API (بس نفس الاسم المستخدم)
+const secondProducts = products.slice(11, 12); // مثلاً إعلان ثابت
+const threProducts = products.slice(10,15 ); // مثلاً إعلان ثابت
+
+// نجمعهم
+const discountProducts = [...firstProducts, ...secondProducts, ...threProducts];
+
+    
+      const discount = 10; // نسبة الخصم
+      const discountA = 50; // نسبة الخصم
+
+  
     return (
-    <>
-    <h1>productlist</h1>
+      <>
+        <h1>Product List</h1>
+        <div style={{ width: '600px', margin: 'auto' }}>
+
+            {secondProducts.map((products, index) => (
+              <SwiperSlide key={index}>
+                <img
+                  src={products.thumbnail}
+                  alt={`Product Image ${index}`}
+                  style={{ width: '100%', height: '350px' }}
+                />
+              </SwiperSlide>
+            ))}
+    </div>
+    <div style={{ display: 'flex', flexWrap: 'wrap', gap: '20px', justifyContent: 'center' }}>
+    {threProducts.map((products, index) => (
+      <div className='discountProducts' key={index}>
+        <div className='discount'>-{discountA}%</div>
+        <img className='detilsimg' src={products.thumbnail} alt={`Product Image ${index}`}/>
+        <div className='detilsprice' >
+          <p className='detilsoldprice'>${products.price}</p>
+          <p className='detilsnewprice'>${(products.price - (products.price * discountA / 100)).toFixed(2)}</p>
+        </div>
+      </div>
+      ))} 
+    </div>
+
+        <div style={{ width: '600px', margin: 'auto' }}>
+          <Swiper
+            modules={[Autoplay]}
+            spaceBetween={10}
+            slidesPerView={1}
+            autoplay={{ delay: 2000 }}
+            loop={true}
+          >
+            {selectedProducts.map((products, index) => (
+              <SwiperSlide key={index}>
+                <img
+                  src={products.thumbnail}
+                  alt={`Product Image ${index}`}
+                  style={{ width: '100%', height: '350px' }}
+                />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+    </div>
+    <div style={{ display: 'flex', flexWrap: 'wrap', gap: '20px', justifyContent: 'center' }}>
+    {firstProducts.map((products, index) => (
+      <div className='discountProducts' key={index}>
+        <div className='discount'>-{discount}%</div>
+        <img className='detilsimg' src={products.thumbnail} alt={`Product Image ${index}`}/>
+        <div className='detilsprice' >
+          <p className='detilsoldprice'>${products.price}</p>
+          <p className='detilsnewprice'>${(products.price - (products.price * discount / 100)).toFixed(2)}</p>
+        </div>
+      </div>
+      ))} 
+    </div>
+
+
     <div className="productlist">
       {products.map((products) => (
         <div key={products.id} className='producthome'>
